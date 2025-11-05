95d74afefd4a3f99ec810f31743e29d272b8afc0
Ranush Vimantha <ranushvimantha1@gmail.com>
2025-11-05 11:34:28 +0530

    Defined Entity Class
    - Created Customer Customer Entity class with properties
    - Creeated Employee Entity class with properties
    - Created Material Entitiy Class with properties
    - Mapped classes to databse tables

---
+++ Material/Material.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WareHouseApp.Materials
+{
+    public class Material
+    {
+        public int MaterialID { get; set; }
+        public string MaterialName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
+
